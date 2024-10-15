b1b8b1195539bdac566e1d92668339d7e65d6f65
ZaiHua <446840401@qq.com>
2024-10-15 17:23:12 +0800

    新增导航

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgZorroModule } from './ng-zorro.module';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgZorroModule],
+  imports: [RouterOutlet, NgZorroModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
